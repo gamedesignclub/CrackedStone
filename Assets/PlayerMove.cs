25f62a63060cfd887204c362350361f1ef7c16fb
gamedesignclub <bjamturley@gmail.com>
2015-02-05 14:42:27 -0500

    backup commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,60 @@
+﻿
+
+using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+
+public class PlayerMove : MonoBehaviour 
+{
+	public float colliderSize = 1;
+	public int jumps = 0;
+	public int maxJumps = 3;
+	public int jumpForce = 500;
+	public int movementForce = 70;
+	public GameObject floor;
+	public KeyCode up;
+	public KeyCode left;
+	public KeyCode right;
+
+	public Animator playerMotionContoller;
+
+	void Update () 
+	{
+		if (isGrounded())
+			jumps = 0;
+
+		if(jumps < maxJumps)
+		{
+			if (Input.GetKeyDown(up)) 
+			{
+				rigidbody2D.AddForce(Vector3.up * (jumpForce + rigidbody2D.velocity.y));
+				jumps++;
+				playerMotionContoller.SetBool("jump", true);
+				//playerMotionContoller.SetBool("jump", false);
+			}
+
+		}
+
+		if (Input.GetKey(left)) 
+		{
+			rigidbody2D.AddForce(Vector3.left * movementForce);
+			playerMotionContoller.SetBool("walk", true);
+		}
+
+		else if (Input.GetKey(right)) 
+		{
+			rigidbody2D.AddForce(Vector3.right * movementForce);
+			playerMotionContoller.SetBool("walk", true);
+		}
+		else
+		{
+			playerMotionContoller.SetBool("walk", false);
+		}
+	}
+
+	bool isGrounded()
+	{
+		return (int)(transform.position.y - floor.transform.position.y)  == 1;
+	}
+}
---
+++ Assets/ResetLevel.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetLevel : MonoBehaviour 
+{
+	public GameObject player1, player2;
+	public double resetZone;
+
+	void Update () 
+	{
+		if (player1.transform.position.y < resetZone)
+		{
+			PlayerPrefs.SetInt("player1", PlayerPrefs.GetInt("player1") + 1);
+			Application.LoadLevel (0);
+		}
+		else if (player2.transform.position.y < resetZone)
+		{
+			PlayerPrefs.SetInt("player2", PlayerPrefs.GetInt("player2") + 1);
+			Application.LoadLevel (0);
+		}
+
+		Debug.Log ("Player 1: " + PlayerPrefs.GetInt("player1") + " Player 2: "+PlayerPrefs.GetInt("player2"));
+	}
+}
