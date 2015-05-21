c92c43a245ecfa8731d970bdb623b13ebb816bc2
gamedesignclub <bjamturley@gmail.com>
2015-05-21 14:25:42 -0400

    Added jump sound

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(AudioSource))]
+
 public class PlayerMove : MonoBehaviour 
 {
 	public float colliderSize = 1;
 	public int jumps = 0;
-	public int maxJumps = 3;
-	public int jumpForce = 500;
+	public static int maxJumps = 3;
+	public int jumpForce = 500, temp;
 	public int movementForce = 70;
 	public GameObject floor;
 	public KeyCode up;
@@ -16,21 +18,30 @@ public class PlayerMove : MonoBehaviour
 	public Animator playerMotionContoller;
 	public Vector3 eulerAngles;
 
+	public AudioClip jump;
+	AudioSource audio;
+	
 	void Start()
 	{
 		eulerAngles = transform.rotation.eulerAngles;
+		temp = jumpForce;
+		audio = GetComponent<AudioSource>();
 	}
 
 
 	void Update () 
 	{
+
 		if (isGrounded())
 			jumps = 0;
+		if (GetComponent<Rigidbody2D> ().velocity.y < 0)
+			jumpForce += 90 * -((int)GetComponent<Rigidbody2D>().velocity.y);
 
-		//Time.timeScale = 5f;
+		Debug.Log ("Force: "+(int)GetComponent<Rigidbody2D> ().velocity.y+" Jump  Force: "+jumpForce);
 
 		if(jumps < maxJumps && Input.GetKeyDown(up))
 		{
+				audio.PlayOneShot(jump);
 				GetComponent<Rigidbody2D>().AddForce(Vector3.up * (jumpForce + GetComponent<Rigidbody2D>().velocity.y));
 				jumps++;
 				playerMotionContoller.SetBool("jump", true);
@@ -56,6 +67,9 @@ public class PlayerMove : MonoBehaviour
 
 		eulerAngles = new Vector3(0, eulerAngles.y, eulerAngles.z);
 		transform.rotation = Quaternion. Euler(eulerAngles);
+
+		//Set jump back
+		jumpForce = temp;
 	}
 
 	bool isGrounded()
