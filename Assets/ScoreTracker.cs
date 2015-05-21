8092c9ef53305c765e59a945fb329fe9e87823f0
gamedesignclub <bjamturley@gmail.com>
2015-05-21 15:10:15 -0400

    Added score system

---
+++ Assets/ScoreTracker.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour 
+{
+
+	public int player1Score = 0, player2Score = 0;
+	public Vector3 player1Pos, player2Pos;
+	public GameObject player1, player2;
+	public Text player1Text, player2Text;
+
+	float a = 0f;
+
+	void Start()
+	{
+		player2Pos = player2.transform.position;
+		player1Pos = player1.transform.position;
+	}
+
+	void Update () {
+		//code
+		if (player1.transform.position.y < -11) 
+		{
+			player2Score++;
+			player1.transform.position = player1Pos;
+			player2.transform.position=  player2Pos;
+		}
+		else if (player2.transform.position.y < -11) 
+		{
+			player1Score ++;
+			player1.transform.position = player1Pos;
+			player2.transform.position = player2Pos;
+		}
+
+		player1Text.text = "" + player1Score;
+		player2Text.text = "" + player2Score;
+
+	}
+
+}
